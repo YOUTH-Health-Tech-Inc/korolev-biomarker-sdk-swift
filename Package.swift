9781e199271f52a7a82a6236d0ad6fdf924fd364
choobea <32361880+choobea@users.noreply.github.com>
2025-02-07 00:01:10 +0700

    Rework of the package

--- Package.swift
+++ Package.swift
@@ -4,35 +4,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "vitl-sdk-plugin",
-    platforms: [
-            .iOS(.v14),
-        ],
+    name: "SDKNativePack",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "vitl-sdk-plugin",
-            type: .dynamic,
-            targets: ["vitl-sdk-plugin"]),
+            name: "SDKNativePack",
+            targets: ["SDKNativePack"]),
     ],
     targets: [
-            .target(
-                name: "vitl-sdk-plugin",
-                dependencies: [
-                    .target(name: "App", condition: .when(platforms: [.iOS])),
-                    .target(name: "Flutter", condition: .when(platforms: [.iOS])),
-                    .target(name: "flutter_inappwebview_ios", condition: .when(platforms: [.iOS])),
-                    .target(name: "OrderedSet", condition: .when(platforms: [.iOS])),
-                ]
-            ),
-            .binaryTarget(name: "App", path: "./Framework/App.xcframework"),
-            .binaryTarget(name: "Flutter", path: "./Framework/Flutter.xcframework"),
-                    .binaryTarget(name: "flutter_inappwebview_ios", path: "./Framework/flutter_inappwebview_ios.xcframework"),
-                    .binaryTarget(name: "OrderedSet", path: "./Framework/OrderedSet.xcframework"),
-         ]
-//    targets: [
-//        .target(
-//            name: "vitl-sdk-plugin",
-//            dependencies: ["App", "Flutter", "biosensesignal_flutter_sdk", "flutter_inappwebview_ios", "OrderedSet","path_provider_foundation", "permission_handler_apple", "share_plus"]),
-//
-//    ]
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SDKNativePack",
+            dependencies: [
+                .target(name: "App", condition: .when(platforms: [.iOS])),
+                .target(name: "Flutter", condition: .when(platforms: [.iOS])),
+                .target(name: "flutter_inappwebview_ios", condition: .when(platforms: [.iOS])),
+                .target(name: "OrderedSet", condition: .when(platforms: [.iOS])),
+                .target(name: "permission_handler_apple", condition: .when(platforms: [.iOS])),
+    
+            ]
+        ),
+        .binaryTarget(name: "App", path: "./Frameworks/App.xcframework"),
+        .binaryTarget(name: "Flutter", path: "./Frameworks/Flutter.xcframework"),
+        .binaryTarget(name: "flutter_inappwebview_ios", path: "./Frameworks/flutter_inappwebview_ios.xcframework"),
+        .binaryTarget(name: "OrderedSet", path: "./Frameworks/OrderedSet.xcframework"),
+        .binaryTarget(name: "permission_handler_apple", path: "./Frameworks/permission_handler_apple.xcframework"),
+
+    ]
 )
